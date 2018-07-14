d1bb32122b66ab782491dd2af47aba33d7ae46d2
jealou$y <wygonski@gmail.com>
2018-07-14 23:18:02 +0200

    moved some styling outside the .home section

--- src/Routes.js
+++ src/Routes.js
@@ -2,15 +2,16 @@ import { h, Component } from 'preact';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './components/Home';
+import Portfolio from './components/Portfolio';
 
 class Routes extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route exact path="/" render={() => <Home />} />
-          <Route path="/portfolio" render={() => <p>portfolio here</p>} />
-          <Route path="/kontakt" render={() => <p>kontakt</p>} />
+          <Route exact path="/" component={Home} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/kontakt" component={Home} />
         </div>
       </BrowserRouter>
     );
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,15 @@
+import { h, Component } from 'preact';
+import Button from './../Button';
+import './index.css';
+
+class Portfolio extends Component {
+  render() {
+    return (
+      <section className="portfolio">
+        <h2>Sprawdź moje poprzednie prace</h2>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
