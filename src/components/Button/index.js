fb0330f741d0f99d67dffed45885fccf8498ee5c
jealou$y <wygonski@gmail.com>
2018-07-14 23:44:28 +0200

    added external link functionality

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,6 +5,8 @@ import './index.css';
 class Button extends Component {
   render() {
     return (
+      this.props.external ? 
+      <a className="btn-outline" href={this.props.to} target="_blank">{this.props.children}</a> :
       <Link className="btn-outline" to={this.props.to}>{this.props.children}</Link>
     );
   }
