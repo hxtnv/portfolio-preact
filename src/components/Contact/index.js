4db0a2e5614ce40ceeb9cfe66fdc46661ac0d487
jealou$y <wygonski@gmail.com>
2018-07-15 16:51:56 +0200

    added working submit form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,6 +3,40 @@ import HomeButton from './../HomeButton';
 import './index.css';
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.sendMessage = this.sendMessage.bind(this);
+  }
+
+  state = {
+    name: '',
+    email: '',
+    message: '',
+    returnMsg: {color: '#fff', msg: ''}
+  }
+
+  sendMessage(e) {
+    e.preventDefault();
+
+    if(!this.state.name || !this.state.email || !this.state.message) return;
+
+    fetch('https://wygonski.eu/send.php', {
+      method: 'post',
+      body: JSON.stringify(this.state)
+    }).then((response) => {
+      return response.json();
+    }).then((data) => {
+      if(data.success) {
+        this.setState({returnMsg: {color: '#5cb85c', msg: 'Wiadomość wysłana - otrzymasz odpowiedź najszybciej jak to możliwe!'}});
+      }else {
+        this.setState({returnMsg: {color: '#d9534f', msg: 'Nie udało się wysłać wiadomości - spróbuj ponownie.'}});
+      }
+    }).catch((e) => {
+      console.log(e);
+      this.setState({returnMsg: {color: '#d9534f', msg: 'Nie udało się wysłać wiadomości - spróbuj ponownie.'}});
+    })
+  }
+
   render() {
     return (
       <section className="contact">
@@ -17,20 +51,22 @@ class Contact extends Component {
         <div className="holder">
           <p className="text-muted">Wiadomość zwrotna przyjdzie na email który podasz w formularzu, dlatego upewnij się że jest poprawny.</p>
           
-          <form method="POST" action="">
+          <form onSubmit={this.sendMessage}>
             <div className="form-50">
               <label>Imię i nazwisko:</label>
-              <input type="text" placeholder="Jan Kowalski" id="name" required />
+              <input type="text" placeholder="Jan Kowalski" onChange={(e) => this.setState({name: e.target.value})} required />
             </div>
 
             <div className="form-50">
               <label>Email:</label>
-              <input type="email" placeholder="przykladowy@email.net" id="email" required />
+              <input type="email" placeholder="przykladowy@email.net" onChange={(e) => this.setState({email: e.target.value})} required />
             </div>
 
             <div className="form-100">
               <label>Wiadomość:</label>
-              <textarea id="message" required></textarea>
+              <textarea id="message" onChange={(e) => this.setState({message: e.target.value})} required></textarea>
+
+              <p style={{color: this.state.returnMsg.color, margin: '0', minHeight: '22px'}}>{this.state.returnMsg.msg}</p>
 
               <input type="submit" value="Wyślij wiadomość" className="btn-outline" />
             </div>
