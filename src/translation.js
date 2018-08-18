522f35f856bfc99fcbf493e428b390870fcff8ea
jealou$y <wygonski@gmail.com>
2018-08-18 12:37:28 +0200

    added lithuanian language

--- src/translation.js
+++ src/translation.js
@@ -84,44 +84,44 @@ module.exports = {
   },
 
   lt: {
-    doc_title: 'Projektowanie i tworzenie stron www',
+    doc_title: 'Interneto svetainių projektavimas ir kūrimas',
 
-    home_desc1: 'Tworzę zaawansowane i eleganckie interfejsy z użyciem najnowszych technologii.',
-    home_desc2: 'Twoje zadowolenie jako klienta jest dla mnie <strong>najważniejsze</strong>.',
-    home_read_more: 'Czytaj więcej...',
-    home_hide_text: 'Schowaj tekst...',
-    home_spec: 'Posiadam wieloletnie doświadczenie z technologiami www (HTML, CSS, JS, PHP, Node), frameworkami typu <strong>React</strong> i <strong>Inferno</strong> a także systemy CMS typu <strong>Wordpress</strong>.',
-    contact: 'Kontakt',
+    home_desc1: 'Kuriu pažangias ir elegantiškas naudotojo sąsajas (UI) naujausių technologijų pagalba.',
+    home_desc2: 'Man <strong>svarbiausia</strong>, kad klientas būtų patenkintas.',
+    home_read_more: 'Daugiau...',
+    home_hide_text: 'Mažiau...',
+    home_spec: 'Daug metų dirbu su www technologija (HTML, CSS, JS, PHP, Node), frameworkais kaip <strong>React</strong> or <strong>Inferno</strong> ir CMS tipo sistemomis kaip <strong>Wordpress</strong>.',
+    contact: 'Kontaktas',
     portfolio: 'Portfolio',
 
-    contact_header: 'Skontaktuj się ze mna:',
-    contact_email_fix: 'Wiadomość zwrotna przyjdzie na email który podasz w formularzu, dlatego upewnij się że jest poprawny.',
-    contact_full_name: 'Imię i nazwisko:',
-    contact_full_name_example: 'Jan Kowalski',
-    contact_email: 'Email',
-    contact_email_example: 'przykladowy@email.net',
-    contact_message: 'Wiadomość: ',
-    contact_send_message: 'Wyślij wiadomość',
+    contact_header: 'Susisiekite su manimi:',
+    contact_email_fix: 'Atsakymas bus išsiųstas Jūsų elektroninio pašto adresu, todėl patikrinkite, ar teisingai jį nurodėte.',
+    contact_full_name: 'Vardas ir pavardė:',
+    contact_full_name_example: 'Vardenis Pavardenis',
+    contact_email: 'El.paštas',
+    contact_email_example: 'elpaštas@email.net',
+    contact_message: 'Tekstas: ',
+    contact_send_message: 'Išsiųsti tekstą',
 
     contact_msg_sent_success: 'Message sent - you will receive a reply as soon as possible!',
     contact_msg_sent_error: 'There\'s been an error. Please try again.',
 
-    portfolio_prev: 'Sprawdź moje poprzednie prace: ',
-    portfolio_see_online: 'Zobacz online',
+    portfolio_prev: 'Įvertinkite mano ankstesnius darbus: ',
+    portfolio_see_online: 'Patikrinkite online', 
 
     months: {
-      jan: 'Styczeń',
-      feb: 'Luty',
-      mar: 'Marzec',
-      apr: 'Kwiecień',
-      may: 'Maj',
-      jun: 'Czerwiec',
-      jul: 'Lipiec',
-      aug: 'Sierpień',
-      sep: 'Wrzesień',
-      oct: 'Październik',
-      nov: 'Listopad',
-      dec: 'Grudzień'
+      jan: 'Sausis',
+      feb: 'Vasaris',
+      mar: 'Kovas',
+      apr: 'Balandis',
+      may: 'Gegužė',
+      jun: 'Birželis',
+      jul: 'Liepa',
+      aug: 'Rugpjūtis',
+      sep: 'Rugsėjis',
+      oct: 'Spalis',
+      nov: 'Lapkritis',
+      dec: 'Gruodis'
     }
   }
 }
